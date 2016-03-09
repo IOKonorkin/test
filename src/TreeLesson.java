7d9560873dad551c80111480aaa1b05a29b8f6c1
Ivan Konorkin <konorkin.ivan@gmail.com>
2016-03-09 17:10:48 +0300

    Новая версия программы.

--- src/TreeLesson.java
+++ src/TreeLesson.java
@@ -7,10 +7,9 @@ import java.util.TreeMap;
 public class TreeLesson {
 	public static void main(String[] args) {
 		Map<String, String> hm = new TreeMap<>();
-		hm.put("Moscow", "Russia");
 		String country = hm.get("Moscow");
-		if (country != null) {
-			System.out.println(country);
+		for (String c : hm.values()) {
+			System.out.println(c);
 		}
 		for (String c : hm.values()) {
 			System.out.println(c);
